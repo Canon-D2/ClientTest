46b21a4f0c0a66b814079aa5b75fc04ef068178e
@chibaodang2002 <chibaodang2002@gmail.com>
2024-11-05 19:21:19 +0700

    EC2

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { VideoPlayer } from "6pp"
+import { useState } from "react"
+
+const App = () => {
+
+  const [quality,setQuality] = useState<number>(480);
+
+  return (
+    <div>
+      <VideoPlayer 
+        captions="" 
+        src ="http://localhost:3000/video" 
+        setQuality = { setQuality }
+      />
+    </div>
+  );
+};
+
+export default App
